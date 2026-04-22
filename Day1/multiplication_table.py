14046de295b8f46e6f9f0e849ea70bd7026f2356
Lucky Shrestha <luckystha92@gmail.com>
2026-04-22 22:38:06 +0545

    Remove Day1 from Day2 branch

--- Day1/multiplication_table.py
+++
@@ -1,9 +0,0 @@
-try:
-    num = int(input("Enter a number: "))
-
-    print("Multiplication Table:")
-    for i in range(1, 11):
-        print(f"{num} x {i} = {num * i}")
-
-except ValueError:
-    print("Invalid input! Please enter a number.")
